5ff31c167b1daf9045a9cc0e40b4ac77af7b6e32
Danny Wang <dannywang@DannydeMBP.home>
2022-10-01 23:09:32 +0800

    add day18 demo

---
+++ Day18/relaxer/script.js
@@ -0,0 +1,20 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const holdTime = totalTime / 5;
+const breatheTime = holdTime * 2;
+
+function breatheAnimation() {
+  text.innerText = "Breathe in!";
+  container.className = "container grow";
+  setTimeout(() => {
+    text.innerText = "Hold it!";
+    setTimeout(() => {
+      text.innerText = "Breathe out!";
+      container.className = "container shrink";
+    }, holdTime);
+  }, breatheTime);
+}
+breatheAnimation();
+setInterval(breatheAnimation, totalTime);
